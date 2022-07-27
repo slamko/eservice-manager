0bc064cde2533520799ee65cb1b49cf1f7bc8f3c
Viacheslav <vaceslavkozin619@gmail.com>
2022-07-27 20:29:08 +0200

    start and kill services

--- eservice-manager.el
+++ eservice-manager.el
@@ -2,13 +2,28 @@
 
 (setq esm/services nil)
 
-(defun start-process-once (name shell-cmd)
+(defun esm/start-process (name shell-cmd &optional dedicated-log-buffer)
   (interactive)
-  (if (not (position-if (lambda (s) (equal name s)) esm/services))
-   (progn (start-process-shell-command name log-buffer shell-cmd)
-		  (setq esm/services (push name esm/services)))))
+  (start-process-shell-command
+		 name
+		 (if dedicated-log-buffer
+			 (get-buffer-create name)
+		   log-buffer)
+		 shell-cmd))
 
-(start-process-once "some" "/bin/pwd")
+(defun esm/start-process-once (name shell-cmd &optional dedicated-log-buffer)
+  (interactive)
+  (if (not (assq (intern name) esm/services))
+	  (progn
+		(let ((proc (esm/start-process name shell-cmd dedicated-log-buffer)))
+		  (setq esm/services (push (cons (intern name) 'proc) esm/services))))))
 
+(defun esm/kill-process (name &optional kill-dedicated-buffer)
+  (interactive)
+  (switch-to-buffer log-buffer)
+  (kill-process (assoc name esm/services))
+  (when kill-dedicated-buffer (kill-buffer name)))
 
+(esm/start-process-once "some" "/bin/pwd")
+(esm/kill-process "some")
 
